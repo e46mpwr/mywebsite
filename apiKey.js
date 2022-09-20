fc594edc9a71db98046321bb85c2e180678b4498
Dennis Au <dennis.hirofumi.au@gmail.com>
2022-09-19 23:43:15 -0700

    Update apiKey.js

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 const apiKey = '1sax2emgqDcct7M9xgqLrEoJG5edaoCC5Nfb8Avhx';
-const appId = ‘app.vercel.neon-ten-mywebsite’;
+const appId = 'app.vercel.neon-ten-mywebsite';
 export default {apiKey, appId};
 
